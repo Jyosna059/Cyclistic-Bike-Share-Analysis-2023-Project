f8e8feb0f31b953085074507504662b5576eb317
Jyosna <111238384+Jyosna059@users.noreply.github.com>
2024-05-18 19:04:11 +0530

    Data Cleaning

---
+++ 3 Data Cleaning.sql
@@ -0,0 +1,71 @@
+#DATA CLEANING
+
+#Use created Database
+USE `cyclistic-bike-sales-analysis`;
+
+#Create new table in existing Database
+CREATE TABLE `cyclistic-bike-sales-analysis`.`cleaned_data` AS (
+WITH ride_length AS (
+    SELECT 
+        ride_id,
+        TIMESTAMPDIFF(MINUTE, started_at, ended_at) AS ride_length
+    FROM 
+        combined_data
+)
+SELECT
+    a.ride_id,
+    rideable_type,
+    started_at,
+    ended_at,
+    ride_length,
+    CASE DAYOFWEEK(a.started_at)
+        WHEN 1 THEN 'SUN'
+        WHEN 2 THEN 'MON'
+        WHEN 3 THEN 'TUES'
+        WHEN 4 THEN 'WED'
+        WHEN 5 THEN 'THURS'
+        WHEN 6 THEN 'FRI'
+        WHEN 7 THEN 'SAT'
+    END AS day_of_week,
+    CASE MONTH(a.started_at)
+        WHEN 1 THEN 'JAN'
+        WHEN 2 THEN 'FEB'
+        WHEN 3 THEN 'MAR'
+        WHEN 4 THEN 'APR'
+        WHEN 5 THEN 'MAY'
+        WHEN 6 THEN 'JUN'
+        WHEN 7 THEN 'JUL'
+        WHEN 8 THEN 'AUG'
+        WHEN 9 THEN 'SEP'
+        WHEN 10 THEN 'OCT'
+        WHEN 11 THEN 'NOV'
+        WHEN 12 THEN 'DEC'
+    END AS month,
+    start_station_name,
+    end_station_name,
+    start_lat,
+    start_lng,
+    end_lat,
+    end_lng,
+    member_casual
+FROM 
+    combined_data a
+JOIN 
+    ride_length b ON a.ride_id = b.ride_id
+);
+
+#delete null rows
+DELETE FROM cleaned_data
+WHERE 
+    start_station_name IS NULL OR start_station_name = ''
+    OR end_station_name IS NULL OR end_station_name = ''
+    OR end_lat IS NULL OR end_lat = ''
+    OR end_lng IS NULL OR end_lng = '';
+
+#MAake ride_id as PRIMARY KEY
+ALTER TABLE cleaned_data 
+ADD PRIMARY KEY(ride_id);
+
+#count the number of rows in a table
+SELECT COUNT(ride_id) AS no_of_rows     
+FROM cleaned_data;
